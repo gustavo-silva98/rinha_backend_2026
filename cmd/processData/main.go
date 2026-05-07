25ee5725c1eb903a9b80b1cc582a40a134d4362f
gustavo-silva98 <silva.gustavo98@gmail.com>
2026-05-07 01:39:47 -0300

    Retirada de timers de validação

--- cmd/processData/main.go
+++ cmd/processData/main.go
@@ -5,18 +5,15 @@ import (
 	"compress/gzip"
 	"encoding/binary"
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"os"
 	"path/filepath"
 	"rinha2026/internal/model"
 	"rinha2026/internal/preprocess"
-	"time"
 )
 
 func main() {
-	t0 := time.Now()
 	os.Chdir("..")
 	os.Chdir("..")
 	workingPath, err := os.Getwd()
@@ -24,31 +21,24 @@ func main() {
 		log.Fatal(err)
 	}
 	path := filepath.Join(workingPath, "resources", "references.json.gz")
-	fmt.Printf("Trocando de Pasta: %v\n", time.Since(t0))
 
-	t1 := time.Now()
 	f, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
-	fmt.Printf("Abrindo Path %v\n", time.Since(t1))
 
-	t2 := time.Now()
 	gz, err := gzip.NewReader(bufio.NewReaderSize(f, 1<<20))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer gz.Close()
-	fmt.Printf("Criando Reader gzip %v\n", time.Since(t2))
 
-	t3 := time.Now()
 	out, err := os.Create("vectors.bin")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer out.Close()
-	fmt.Printf("Criação de arquivo %v\n", time.Since(t3))
 	bw := bufio.NewWriterSize(out, 1<<20)
 
 	bw.WriteString(preprocess.Magic)
@@ -62,7 +52,6 @@ func main() {
 	var idx int32
 	var r model.RawVector
 
-	t4 := time.Now()
 	for dec.More() {
 		if err := dec.Decode(&r); err != nil {
 			log.Fatalf("erro ao decodificar vetor idx: %v - %v", idx, err)
@@ -87,11 +76,8 @@ func main() {
 	if err := bw.Flush(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Decodificação feita %v\n", time.Since(t4))
 	if _, err := out.Seek(8, io.SeekStart); err != nil {
 		log.Fatal(err)
 	}
 	binary.Write(out, binary.LittleEndian, uint32(idx))
-	log.Printf("Concluído: %d vetores: tempo:%v", idx, time.Since(t0))
-
 }
